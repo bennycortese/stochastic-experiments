cc8943af9d35425bc54f596dd7ef892ca9215233
Benny Cortese <bennycortese@gmail.com>
2024-06-09 23:53:12 -0700

    Better codebad idea and remove useless comment

--- quiz_study.py
+++ quiz_study.py
@@ -6,8 +6,7 @@ import scipy.stats as stats
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-# Rock soup
-# Note to self, look into codepad api
+# Note to self, codepad sdk maybe?
 def compose_random_walks(walks):
     composed_walk = []
     for walk in walks:
